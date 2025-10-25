7fc21f8a9e93bd838374ddccded5d215976dd08d
Abhay Maurya <abhaymaurya821@gmail.com>
2025-10-25 16:11:31 +0530

    imagekit added and Backend completed

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,84 @@
 import { NextAuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from 'bcryptjs';
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "email", type: "text", placeholder: "jsmith@123.com" },
+        password: { label: "Password", type: "password" }
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          throw new Error("Missing username or password");
+        }
+
+        try {
+          await connectToDatabase();
+          
+          // Find user by email
+          const user = await User.findOne({ email: credentials.username });
+          
+          if (!user) {
+            throw new Error("User not found");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isValid) {
+            throw new Error("Invalid password");
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email
+          };
+          
+        } catch (error) {
+          // Return null on error to indicate auth failed
+          console.error("Authentication error:", error);
+          return null;
+        }
+      }
+    })
   ],
+
+
+  callbacks:{
+    async jwt({token, user}){
+      if(user){
+        token.id = user.id;
+      }
+      return token;
+    },
+
+     async session({session, token}){
+      if(session.user){
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+
+  },
+
+
+  pages:{
+    signIn:"/login",
+    error:"/login",
+  },
+
+  session:{
+    strategy:"jwt",
+    maxAge:30 * 24 * 60 * 60,
+  },
+
+  secret:process.env.NEXTAUTH_SECRET
 };
